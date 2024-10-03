d97c68ba445d0e90ddbec681d0fda2a47e351913
Oluwafemi21 <olukoyaoluwafemi14@gmail.com>
2024-10-03 15:21:11 +0100

    initialize project

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,45 @@
+<template>
+  <Teleport to="body">
+    <Transition>
+      <div v-if="showModal" class="modal-container" @click="close">
+        <div class="modal" @click.stop>
+          <button class="modal_close_button" @click="close">
+            <CloseIcon class="size-[20px] text-[#64748B]" />
+          </button>
+          <slot name="body" />
+        </div>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+import CloseIcon from '@/components/icons/IconCancel'
+const emits = defineEmits(['closeModal'])
+defineProps<{
+  showModal: boolean
+}>()
+
+const close = () => {
+  emits('closeModal')
+}
+</script>
+
+<style scoped>
+/* Common styles for all positions */
+.v-enter-from {
+  opacity: 0;
+}
+
+.v-enter-active {
+  transition: all 20ms ease-in-out;
+}
+
+.v-enter-to {
+  opacity: 1;
+}
+
+.v-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/components/PhotoCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="photo_card_container">
+    <div class="photo_card">
+        <template v-if="!loading">
+            <img :src="imageInfo.url"/>
+        <div class="photo_card_content">
+            <p>{{ imageInfo.title }}</p>
+            <span>{{ imageInfo.location }}</span>
+        </div>
+        </template>
+        <div v-else class="content_loading_state">
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ImageInfo } from '@/utils/image.types.ts'
+defineProps<{
+    loading: boolean,
+    imageInfo: ImageInfo
+}>()
+</script>
+
+<style scoped lang="scss">
+.photo_card_container {
+    border-radius: 8px;
+    
+    &.loading{
+        padding:8px;
+        background-color: #ccc;
+
+        .photo_card {
+            .content_loading_state {
+                position:absolute;
+                bottom: 0;
+                 &:first {
+                    height: 16px;
+                    width:150px;
+                    background-color: rgba($color: #ccc, $alpha: 0.7);
+                 }
+            }
+        }
+    }
+
+    .photo_card {
+        position:relative;
+
+        &_content{ 
+            p {
+                font-weight: 500;
+                font-size: 16px;
+                margin-bottom:6px;
+            }
+
+            span {
+                font-size: 14px;
+            }
+        }
+    }
+}
+</style>
